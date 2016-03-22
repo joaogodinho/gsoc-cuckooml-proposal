da64adeea3873acc0f571c24613e3006ec76548c
João Godinho <joaocfgodinho@gmail.com>
2016-03-22 19:05:26 +0000

    Provides more info about the samples

--- analyze_report.py
+++ analyze_report.py
@@ -1,5 +1,6 @@
 from os import listdir
 from os.path import join
+from collections import Counter
 import json
 
 
@@ -14,20 +15,49 @@ def listReportsPath():
     return paths
 
 
+# Returns a list of json objects
+def loadSamples(reports):
+    objs = []
+    for r in reports:
+        with open(r, 'r') as file:
+            objs.append(json.load(file))
+    return objs
+
+
 # Returns a set with all top level keys seen
 def getSeenKeys(reports):
     keys = set()
     for r in reports:
-        with open(r, 'r') as file:
-            jsonObj = json.load(file)
-            keys.update(jsonObj.keys())
+        keys.update(r.keys())
     return keys
+
+
+# Counts the number of top level keys that contain information
+# some keys might contain no information and they're still defined
+# so need to manually filter them
+def getKeysDist(reports):
+    keysList = []
+    for r in reports:
+        for k in r.keys():
+            if k == u'virustotal':
+                if 'scans' in r[k].keys():
+                    keysList.append(k)
+                else:
+                    pass
+            elif len(r[k]) > 0:
+                keysList.append(k)
+    return Counter(keysList)
 
 
 def main():
     reports = listReportsPath()
-    keysSet = getSeenKeys(reports)
+    samplesSize = len(reports)
+    jsonObjs = loadSamples(reports)
+    keysSet = getSeenKeys(jsonObjs)
+    keysDist = getKeysDist(jsonObjs)
     print('Top level keys seen: {}'.format(str(keysSet)))
+    for k, v in keysDist.items():
+        print('{} appears in {} samples ({:.2f}%)'.format(k, v, float(v) / samplesSize * 100))
 
 if __name__ == '__main__':
     main()
