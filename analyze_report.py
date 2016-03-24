5e6627fbda54da1b4f449aa5a044d847887a424b
João Godinho <joaocfgodinho@gmail.com>
2016-03-24 15:49:14 +0000

    Stats on the samples completed

--- analyze_report.py
+++ analyze_report.py
@@ -39,7 +39,7 @@ def getKeysDist(reports):
     keysList = []
     for r in reports:
         for k in r.keys():
-            if k == u'virustotal':
+            if k == 'virustotal':
                 if 'scans' in r[k].keys():
                     keysList.append(k)
                 else:
@@ -49,15 +49,74 @@ def getKeysDist(reports):
     return Counter(keysList)
 
 
+# Returns all possible normalized virustotal values
+def getVirusNorm(reports):
+    normList = []
+    for r in reports:
+        try:
+            norm = r['virustotal']['normalized']
+            normList += norm
+        except:
+            # Sample was not scanned
+            pass
+    return Counter(normList)
+
+
+# Returns all possible signatures
+def getSignatures(reports):
+    sigList = []
+    for r in reports:
+        try:
+            sig = r['signatures']
+            for s in sig:
+                sigList.append(s['name'])
+        except:
+            # No signature key
+            pass
+    return Counter(sigList)
+
+
+# Returns all possible api calls
+def getApiCalls(reports):
+    apiList = []
+    for r in reports:
+        try:
+            apistats = r['behavior']['apistats']
+            for proc in apistats.keys():
+                for call in apistats[proc]:
+                    apiList.append(call)
+        except:
+            # No behavior or apistat
+            pass
+    return Counter(apiList)
+
+
 def main():
     reports = listReportsPath()
     samplesSize = len(reports)
     jsonObjs = loadSamples(reports)
     keysSet = getSeenKeys(jsonObjs)
     keysDist = getKeysDist(jsonObjs)
-    print('Top level keys seen: {}'.format(str(keysSet)))
+    virusNorm = getVirusNorm(jsonObjs)
+    sigs = getSignatures(jsonObjs)
+    apiCalls = getApiCalls(jsonObjs)
+    print('Top level keys seen: {}\n'.format(str(keysSet)))
+
     for k, v in keysDist.items():
         print('{} appears in {} samples ({:.2f}%)'.format(k, v, float(v) / samplesSize * 100))
+
+    print('\nNumber of unique families: {}'.format(len(virusNorm)))
+    print('Ten most common families:')
+    print(virusNorm.most_common(10))
+
+    print('\nNumber of unique signatures: {}'.format(len(sigs)))
+    print('Ten most common signatures:')
+    print(sigs.most_common(10))
+
+    print('\nNumber of unique apicalls: {}'.format(len(apiCalls)))
+    print('Ten most common calls:')
+    print(apiCalls.most_common(10))
+
 
 if __name__ == '__main__':
     main()
